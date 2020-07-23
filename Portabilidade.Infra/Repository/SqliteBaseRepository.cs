6bf2beff40efe9c8702afd225330640b11560ae4
Rene Felix Correa <renefc3@gmail.com>
2020-07-23 10:14:30 -0300

    Update SqliteBaseRepository.cs

--- Portabilidade.Infra/Repository/SqliteBaseRepository.cs
+++ Portabilidade.Infra/Repository/SqliteBaseRepository.cs
@@ -4,17 +4,34 @@ using Microsoft.Data.Sqlite;
 
 namespace Portabilidade.Infra.Repository
 {
+    /*public interface IDatabaseConnectionFactory{
+        IDbConnection Get();
+    }
+    
+    public class SqliteDatabaseConnectionFactory :IDatabaseConnectionFactory {
+        protected string DbFile
+        {
+            get { return Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\Portabilidade.Infra\Data\Portabilidade.sqlite")); }
+        }
+        
+         protected  IDbConnection Get() => new SqliteConnection("Data Source=" + DbFile);
+    }*/
+
     public class SqliteBaseRepository
     {
-        // Sugestão para diminuir o acesso a essa propriedade
-        // protected string DbFile
+        //protected IDatabaseConnectionFactory _connectionFactory;
+        
+        //protected SqliteBaseRepository(IDatabaseConnectionFactory connectionFactory){
+        //    _connectionFactory = connectionFactory;
+        //}
+        
+        // Remover propriedade Usar o IDatabaseConnectionFactory 
         public static string DbFile
         {
             get { return Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\Portabilidade.Infra\Data\Portabilidade.sqlite")); }
         }
         
-        // Sugestão para diminuir o acesso a essa função 
-        // protected SqliteConnection SimpleDbConnection() => new SqliteConnection("Data Source=" + DbFile);
+        // Remover esse metodo Usar o IDatabaseConnectionFactory 
         public static SqliteConnection SimpleDbConnection() => new SqliteConnection("Data Source=" + DbFile);
     }
 }
