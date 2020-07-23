f464cf1a43ee558dcfd0a01258bcaed28609a77f
Rene Felix Correa <renefc3@gmail.com>
2020-07-23 07:22:01 -0300

    Update SqliteBaseRepository.cs

--- Portabilidade.Infra/Repository/SqliteBaseRepository.cs
+++ Portabilidade.Infra/Repository/SqliteBaseRepository.cs
@@ -6,11 +6,15 @@ namespace Portabilidade.Infra.Repository
 {
     public class SqliteBaseRepository
     {
+        // Sugestão para diminuir o acesso a essa propriedade
+        // protected string DbFile
         public static string DbFile
         {
             get { return Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\Portabilidade.Infra\Data\Portabilidade.sqlite")); }
         }
-
+        
+        // Sugestão para diminuir o acesso a essa função 
+        // protected SqliteConnection SimpleDbConnection() => new SqliteConnection("Data Source=" + DbFile);
         public static SqliteConnection SimpleDbConnection() => new SqliteConnection("Data Source=" + DbFile);
     }
 }
