78628b523a3076258ad525ea83e02ca104cefa3b
Rene Felix Correa <renefc3@gmail.com>
2020-07-23 10:26:34 -0300

    Update ClienteValidator.cs

--- Portabilidade.Domain/Entities/ClienteValidator.cs
+++ Portabilidade.Domain/Entities/ClienteValidator.cs
@@ -5,12 +5,27 @@ using Portabilidade.Service.Util;
 
 namespace Portabilidade.Domain.Entities
 {
-
-    public class ClienteValidator : AbstractValidator<Cliente>
+    /*public interface IClienteValidator : IValidator<Cliente>
     {
+    }*/
 
-        private IValidarStrategy validarDocumento;
+    public class ClienteValidator : AbstractValidator<Cliente>//, IClienteValidator
+    {
+        /*public ClienteValidator(IValidarStrategy validarDocumento)
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("Por Favor, Informe o Nome do Cliente")
+                .Length(3, 150).WithMessage("O campo nome deve ter entre 3 e 150 caracteres");
 
+
+            RuleFor(x => x.DocumentoCpf)
+                .NotEmpty().WithMessage("Por Favor, Informe o Documento")
+                .Must(validarDocumento.Validar).WithMessage("Por Favor, Documento Corretamente !");
+
+            RuleFor(x => x.Endereco).Length(20, 250).WithMessage("Por Favor, Endereço de 20 a 250 Caracteres");
+
+        }*/
+        
         public ClienteValidator()
         {
             RuleFor(x => x.Nome)
